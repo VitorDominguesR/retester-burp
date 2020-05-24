588b8e0445ddf90c8ea0da52a439007b678b7ed3
Vitor Domingues Ribeiro <vitordominguesr@MBP-de-Vitor.lan>
2020-05-23 22:26:15 -0300

    Add: Plans to continue the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Burp "retester" simple script
+
+This tool allows you to fast test vulnarabilities that you found with Burp
---
+++ http-parser.py
@@ -0,0 +1,19 @@
+import json
+
+class BurpRequestParser():
+    
+    def __init__(self, path):
+        self.path = path
+        pass
+        parse_request(self.path)
+
+    def get_request_url(self):
+        pass
+    def get_headers(self):
+        pass
+    def get_payload(self):
+        pass
+    def get_method(self):
+        pass
+    def parse_request(self, path):
+        pass
